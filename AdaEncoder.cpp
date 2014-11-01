169b67af33db6d142605a4fdee68aaca5f383e45
Mike Schwager <mike@schwager.com>
2014-11-01 03:04:49 -0500

    Version 0.8-rc2. The only change from 0.8 is that I'm using the Apache license, and I'm tagging the release in Git.

--- AdaEncoder.cpp
+++ AdaEncoder.cpp
@@ -4,22 +4,19 @@
   AdaEncoder.cpp - A library for reading from Lady Ada's or 
   Sparkfun's rotary encoder.
 
-  Version 0.8 Fri Dec 21 13:17:44 CST 2012
+Copyright 2011,2012,2013,2014 Michael Anthony Schwager
 
-  Copyright 2011 Michael Schwager (aka, "GreyGnome").
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    http://www.apache.org/licenses/LICENSE-2.0
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
 	Questions?  Send mail to mschwage@gmail.com, or see the AdaEncoder Google code page:
 	http://code.google.com/p/adaencoder/
--- Examples/MyEncoder/MyEncoder.ino
+++ Examples/MyEncoder/MyEncoder.ino
@@ -1,4 +1,27 @@
 // Version 1.1: OO version
+/*
+ MyEncoder.ino - A library for reading Lady Ada's or 
+ Sparkfun's rotary encoder.
+ Should work for any rotary encoder with 2 pins (4 states).
+
+Copyright 2011,2012,2013,2014 Michael Anthony Schwager
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+	Questions?  Send mail to mschwage@gmail.com
+
+*/
+
 #include <ByteBuffer.h>
 #include <ooPinChangeInt.h> // necessary otherwise we get undefined reference errors.
 #define DEBUG
